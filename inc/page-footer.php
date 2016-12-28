e6caa2cd31a31cf62e480dba14d1c46bbc8dacd7
Jason Sohl <jason@frozensoliddesigns.com>
2016-12-28 11:25:19 -0800

    adding footer

--- inc/page-footer.php
+++ inc/page-footer.php
@@ -1,5 +1,10 @@
 		</section>
+
+		<footer>
+			<div>Some of our work...</div>
+			<a href="http://oaksofhope.com">Oaks of Hope</a> | <a href="http://venegasdairy.com">Venegas Dairy</a> | <a href="http://fhl.me">Faith, Hope and Love Assisted Living</a>
+		</footer>
 		<!-- Scripts -->
-	
+
 	</body>
 </html>
--- inc/page-header.php
+++ inc/page-header.php
@@ -9,11 +9,11 @@
 		<meta name="keywords" content="<?php echo $local_keywords; ?>">
 		<meta name="google-site-verification" content="uTIzu1DUwuFNnwHIMaU-JaiPU2m9eTRLCPA9J-V8qAc" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		
+
 		<title>
 			<?php echo $local_title; ?>
 		</title>
-		
+
 		<link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,300,700' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" href="/css/fsd-screen.css" />
 		<link rel="shortcut icon" href="/favicon.ico" >
@@ -21,9 +21,9 @@
 	<body>
 		<header class="main-header light btcf">
 			<span class="fl">frozen solid designs</span>
-			<nav class="fr navigation">
+			<!-- <nav class="fr navigation">
 				<a href="/" class="<?php if ( $local_pageCode == "main" ) { echo "selected"; } ?>">Home</a> <b>.</b> 
 				<a href="/contact/" class="<?php if ( $local_pageCode == "contact" ) { echo "selected"; } ?>">Contact</a>
-			</nav>
+			</nav> -->
 		</header>
 		<section class="main-content">
