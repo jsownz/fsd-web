45473f873a9d6d2fd72a265d4165a2b01aeaacdc
Jason Sohl <jason@frozensoliddesigns.com>
2016-12-28 11:40:06 -0800

    another footer break

--- inc/page-footer.php
+++ inc/page-footer.php
@@ -3,7 +3,7 @@
 		<footer>
 			<div>Some of our work...</div>
 			<a href="http://oaksofhope.com" target="_blank">Oaks of Hope</a> | <a href="http://venegasdairy.com" target="_blank">Venegas Dairy</a> | <a href="http://fhl.me" target="_blank">Faith, Hope and Love Assisted Living</a> | Fastpass Tracker <a href="https://play.google.com/store/apps/details?id=com.frozensoliddesigns.fpt&hl=en" target="_blank">(Android)</a>
-			<br />
+			<br /><br />
 			<div>
 				<a href="mailto:jason@frozensoliddesigns.com">jason@frozensoliddesigns.com</a><br />
 				951.347.9276
