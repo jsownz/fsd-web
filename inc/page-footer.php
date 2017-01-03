fe0f83aac3f5d9c54ed57fc02abfcd02017bf041
Jason Sohl <jason@frozensoliddesigns.com>
2017-01-03 09:54:47 -0800

    changing wording in footer also pulling css from sass again

--- inc/page-footer.php
+++ inc/page-footer.php
@@ -1,7 +1,7 @@
 		</section>
 
 		<footer>
-			<div>Some of our work...</div>
+			<div>Some of my work...</div>
 			<a href="http://oaksofhope.com" target="_blank">Oaks of Hope</a> | <a href="http://venegasdairy.com" target="_blank">Venegas Dairy</a> | <a href="http://fhl.me" target="_blank">Faith, Hope and Love Assisted Living</a> | Fastpass Tracker <a href="https://play.google.com/store/apps/details?id=com.frozensoliddesigns.fpt&hl=en" target="_blank">(Android)</a> <a href="https://itunes.apple.com/us/app/fastpass-tracker/id1190206587?ls=1" target="_blank">(iPhone) 
 			<br /><br />
 			<div>
