471dd52790d518f7b88e88e10c02c02f4a9b7110
Jason Sohl <jason@frozensoliddesigns.com>
2017-10-02 13:17:35 -0700

    adding some more sites

--- inc/page-footer.php
+++ inc/page-footer.php
@@ -2,7 +2,7 @@
 
 		<footer>
 			<div>Some of my work...</div>
-			<a href="http://oaksofhope.com" target="_blank">Oaks of Hope</a> | <a href="http://venegasfarms.com" target="_blank">Venegas Family Farms</a> | <a href="http://fhl.me" target="_blank">Faith, Hope and Love Assisted Living</a> | <a href="http://westcoastfleet.com" target="_blank">West Coast Fleet Services</a> | <a href="/buttons">Easy CSS Buttons</a>
+			<a href="https://oaksofhope.com" target="_blank">Oaks of Hope</a> | <a href="https://www.venegasfarms.com" target="_blank">Venegas Family Farms</a> | <a href="http://fhl.me" target="_blank">Faith, Hope and Love Assisted Living</a> | <a href="https://westcoastfleet.com" target="_blank">West Coast Fleet Services</a> | <a href="https://nancylandrum.com" target="_blank">Nancy Landrum</a> | <a href="/buttons">Easy CSS Buttons</a>
 			<br /><br />
 			<div>
 				<a href="mailto:jason@frozensoliddesigns.com">jason@frozensoliddesigns.com</a><br />
